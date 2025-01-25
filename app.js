78276f7a3fc8b05d3e530b7f70c7c74633e40bc8
garyc <garypotato666@gmail.com>
2025-01-25 14:56:29 +1100

    move page data to global data, and add eventListen that page can monitor globalData update

--- app.js
+++ app.js
@@ -1,19 +1,74 @@
-// app.js
 App({
-  onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-  },
   globalData: {
-    userInfo: null
-  }
-})
+    newsLoading: true,
+    newsList: [],
+  },
+  // Event system
+  eventListeners: {},
+  // Add an event listener
+  addEventListener(eventName, callback) {
+    if (!this.eventListeners[eventName]) {
+      this.eventListeners[eventName] = [];
+    }
+    this.eventListeners[eventName].push(callback);
+  },
+  // Remove an event listener
+  removeEventListener(eventName, callback) {
+    if (this.eventListeners[eventName]) {
+      this.eventListeners[eventName] = this.eventListeners[eventName].filter(
+        (cb) => cb !== callback
+      );
+    }
+  },
+  // Trigger an event
+  triggerEvent(eventName, data) {
+    if (this.eventListeners[eventName]) {
+      this.eventListeners[eventName].forEach((callback) => callback(data));
+    }
+  },
+  // Update globalData and trigger events
+  updateGlobalData(newData) {
+    Object.assign(this.globalData, newData);
+    this.triggerEvent('globalDataChange', this.globalData);
+  },
+  onLaunch() {
+    const config = require('./config.js');
+    // get collections
+    wx.request({
+      url: `${config.shopify_admin_URL}/api/collections`,
+      method: 'GET',
+      header: {
+        'Content-Type': 'application/json',
+        cookie: config._vercel_jwt,
+      },
+      success: (res) => {
+        const newsCollection = res.data.data.find((collection) => collection.handle === 'news');
+        if (newsCollection) {
+          wx.request({
+            url: `${config.shopify_admin_URL}/api/products`,
+            method: 'POST',
+            header: {
+              'Content-Type': 'application/json',
+              cookie: config._vercel_jwt,
+            },
+            data: { collection_id: newsCollection.id, limit: 4 },
+            success: (res) => {
+              this.updateGlobalData({
+                newsList: res.data.data,
+                newsLoading: false,
+              });
+            },
+            fail: () => {
+              this.updateGlobalData({ newsLoading: false });
+            },
+          });
+        } else {
+          this.updateGlobalData({ newsLoading: false });
+        }
+      },
+      fail: () => {
+        this.updateGlobalData({ newsLoading: false });
+      },
+    });
+  },
+});
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,130 +1,28 @@
 Page({
-  data: {
+  data:{
     newsLoading: true,
     newsList: [],
-    lessonLoading: true,
-    lessonList: [],
-    sitterLoading: true,
-    sitterList: [],
   },
-  onLoad: function () {
-    const config = require('../../config.js');
-  
-    // First wx.request
-    wx.request({
-      url: `${config.shopify_admin_URL}/api/collections`,
-      method: 'GET',
-      header: {
-        'Content-Type': 'application/json',
-        'cookie': config._vercel_jwt,
-      },
-      success: (res) => {
-        // get sitter collectionapi/products
-        const sitterCollection = res.data.data.find(collection => collection.handle === 'sitter');
-        if (!sitterCollection) {
-          console.warn('Sitter collection not found');
-          return;
-        }
-        const sitterCollectionId = sitterCollection.id;
-        // get sitter collection products
-        wx.request({
-          url: `${config.shopify_admin_URL}/api/products`,
-          method: 'POST',
-          header: {
-            'Content-Type': 'application/json',
-            'cookie': config._vercel_jwt,
-          },
-          data: {
-            "collection_id": sitterCollectionId,
-            "limit": 10
-          },
-          success: (res) => {
-            this.setData({
-              sitterList: res.data.data,
-              sitterLoading: false
-            });
-          },
-          fail: (err) => {
-            console.error('Second request failed:', err);
-          },
-        });
-
-        // get news collection
-        const newsCollection = res.data.data.find(collection => collection.handle === 'news');
-        if (!newsCollection) {
-          console.warn('Sitter collection not found');
-          return;
-        }
-        const newsCollectionId = newsCollection.id;
-        // get news collection products
-        wx.request({
-          url: `${config.shopify_admin_URL}/api/products`,
-          method: 'POST',
-          header: {
-            'Content-Type': 'application/json',
-            'cookie': config._vercel_jwt,
-          },
-          data: {
-            "collection_id": newsCollectionId,
-            "limit": 4
-          },
-          success: (res) => {
-            this.setData({
-              newsList: res.data.data,
-              newsLoading: false
-            });
-          },
-          fail: (err) => {
-            console.error('Second request failed:', err);
-            this.setData({
-              newsLoading: false
-            })
-          },
-        });
-
-        // get lesson collection
-        const lessonCollection = res.data.data.find(collection => collection.handle === 'lessons');
-        if (!lessonCollection) {
-          console.warn('Lesson collection not found');
-          return;
-        }
-        const lessonCollectionId = lessonCollection.id;
-        // get lesson collection products
-        wx.request({
-          url: `${config.shopify_admin_URL}/api/products`,
-          method: 'POST',
-          header: {
-            'Content-Type': 'application/json',
-            'cookie': config._vercel_jwt,
-          },
-          data: {
-            "collection_id": lessonCollectionId,
-            "limit": 4
-          },
-          success: (res) => {
-            this.setData({
-              lessonList: res.data.dat,
-              lessonLoading: false
-            });
-          },
-          fail: (err) => {
-            console.error('Second request failed:', err);
-            this.setData({
-              lessonLoading: false
-            })
-          },
-        });
-      },
-      fail: (err) => {
-        console.error('First request failed:', err);
-      },
+  onLoad() {
+    const app = getApp();
+    // Initialize with globalData
+    this.setData({
+      newsLoading: app.globalData.newsLoading,
+      newsList: app.globalData.newsList,
     });
+    // Add an event listener for globalData changes
+    this.globalDataChangeListener = (updatedGlobalData) => {
+      this.setData({ 
+        newsLoading: updatedGlobalData.newsLoading,
+        newsList: updatedGlobalData.newsList, 
+      });
+    };
+    app.addEventListener('globalDataChange', this.globalDataChangeListener);
   },
-  
-  handleItemClick(e) {
-    const id = e.currentTarget.dataset.id;
-    wx.navigateTo({
-      url: `/pages/nanny/nanny?id=${id}`
-    });
-  }
-})
+
+  onUnload() {
+    const app = getApp();
+    // Remove the event listener when the page is unloaded
+    app.removeEventListener('globalDataChange', this.globalDataChangeListener);
+  },
+});
